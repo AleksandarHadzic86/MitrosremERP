dd3e877a750f83877e6daa47c9c67f2bf2b5d2c9
Aleksandar Hadzic <aleksandarhadzic@hotmail.com>
2023-10-05 14:47:40 +0200

    Email Identity User Personal email

--- MitrosremERP/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ MitrosremERP/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -67,9 +67,10 @@ namespace MitrosremERP.Areas.Identity.Pages.Account.Manage
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
-            [Display(Name = "New email")]
+            [Required(ErrorMessage = "Email obavezan")]
+            [EmailAddress(ErrorMessage = "Email adresa nije validna")]
+            [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Email adresa nije validna.")]
+            [Display(Name = "Nova Email adresa")]
             public string NewEmail { get; set; }
         }
 
@@ -91,7 +92,7 @@ namespace MitrosremERP.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Nije moguce ucitati korisnika sa ID '{_userManager.GetUserId(User)}'.");
             }
 
             await LoadAsync(user);
@@ -103,7 +104,7 @@ namespace MitrosremERP.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Nije moguce ucitati korisnika sa ID '{_userManager.GetUserId(User)}'.");
             }
 
             if (!ModelState.IsValid)
@@ -125,14 +126,14 @@ namespace MitrosremERP.Areas.Identity.Pages.Account.Manage
                     protocol: Request.Scheme);
                 await _emailSender.SendEmailAsync(
                     Input.NewEmail,
-                    "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    "Potvrdite vasu Email adresu",
+                    $"Molim vas potvrdite vas nalog <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>klikni</a>.");
 
-                StatusMessage = "Confirmation link to change email sent. Please check your email.";
+                StatusMessage = "Konfirmacioni link za izmenu vase email adrese je poslat. Proverite vasu email adresu.";
                 return RedirectToPage();
             }
 
-            StatusMessage = "Your email is unchanged.";
+            StatusMessage = "Vasa e-posta je nepromenjena";
             return RedirectToPage();
         }
 
@@ -141,7 +142,7 @@ namespace MitrosremERP.Areas.Identity.Pages.Account.Manage
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+                return NotFound($"Nije moguce ucitati korisnika sa ID '{_userManager.GetUserId(User)}'.");
             }
 
             if (!ModelState.IsValid)
@@ -161,10 +162,10 @@ namespace MitrosremERP.Areas.Identity.Pages.Account.Manage
                 protocol: Request.Scheme);
             await _emailSender.SendEmailAsync(
                 email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                "Potvrdite e-postu",
+                $"Molim vas potvrdite vas nalog <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>klikni</a>.");
 
-            StatusMessage = "Verification email sent. Please check your email.";
+            StatusMessage = "Verifikaciona e-posta je poslata, Molim vas proverite e-postu.";
             return RedirectToPage();
         }
     }
