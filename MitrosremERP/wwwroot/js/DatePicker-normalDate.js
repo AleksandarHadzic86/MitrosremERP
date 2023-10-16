4734742baf136eebed691bacbf9d2b5b5e906974
Aleksandar Hadzic <aleksandarhadzic@hotmail.com>
2023-10-16 09:55:41 +0200

    Nadogradjeno

---
+++ MitrosremERP/wwwroot/js/DatePicker-normalDate.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $('.datepickerNormalDate').datepicker({
+        dateFormat: 'dd.MM.yy',
+        yearRange: "-100:+50",
+        autoSize: true,
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        changeMonth: true,
+        changeYear: true,
+        regional: "sr-SR",
+        maxDate: new Date()
+    });
+});
